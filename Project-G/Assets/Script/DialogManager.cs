15e387ce04616c1ac2f3c2c3b36ee4e417912e15
lhc0724 <lhc0724@gmail.com>
2021-09-19 20:57:34 +0900

    스레드 핸들러 추가

--- Project-G/Assets/Script/DialogManager.cs
+++ Project-G/Assets/Script/DialogManager.cs
@@ -11,24 +11,46 @@ public class DialogManager : MonoBehaviour
     // Start is called before the first frame update
     List<DataManager> DialogList = new List<DataManager> ();
     XmlManager xmlParser;
+
+    EventWaitHandle thd_started = new EventWaitHandle(false, EventResetMode.ManualReset);
+    EventWaitHandle thd_exited = new EventWaitHandle(false, EventResetMode.ManualReset);
+
     private string _sceneName;
+
+    public bool isThdStarted = true;
+    public bool isThdExited = true;
     
     void Start()
     {
         xmlParser = new XmlManager(Application.dataPath + "/Xml");
         _sceneName = SceneManager.GetActiveScene().name;
-        new Thread (() => getCurrSceneDialog()).Start();
+        //new Thread (() => getCurrSceneDialog()).Start();
+
+        Thread thd = new Thread(getCurrSceneDialog);
+        thd.Start((thd_started, thd_exited));
+
+        Debug.Log($"{isThdStarted}, {isThdExited}");
+
+        isThdStarted = thd_started.WaitOne(0);
+        isThdExited = thd_exited.WaitOne(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log($" => {isThdStarted}, {isThdExited}");
     }
 
-    void getCurrSceneDialog()
+    void getCurrSceneDialog(object obj)
     {
-        xmlParser.xmlName = "/Text.xml";
-        DialogList = xmlParser.LoadData(_sceneName);
+        var events = (System.ValueTuple<EventWaitHandle, EventWaitHandle>) obj;
+        {
+            events.Item1.Set();
+
+            xmlParser.xmlName = "/Text.xml";
+            DialogList = xmlParser.LoadData(_sceneName);
+
+            events.Item2.Set();
+        }
     }
 }
