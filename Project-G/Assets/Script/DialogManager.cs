3bb14523af75958cca54200552d28ac0ceb7891b
lhc0724 <lhc0724@gmail.com>
2021-10-08 01:54:52 +0900

    부모 오브젝트가 자식 오브젝트를 관리하도록 수정
    이전 텍스트박스에 따로 스크립트를 두었으나 부모 오브젝트에서 전체적으로 관리할 수 있게 변경.
    버튼 리스너 추가하여 스크립트에서 onClick 관리.
    dialog 출력은 외부에서 request만 줄 수 있으며 직접 접근 불가.
    request시에 유효한 dialog 데이터가 있을 경우 출력.
    화면에 텍스트 출력 중 스킵버튼은 키보드입력(space)또한 받을 수 있게 이벤트 리스너를 코루틴에 추가
    
    플레그 변수 최소화.

--- Project-G/Assets/Script/DialogManager.cs
+++ Project-G/Assets/Script/DialogManager.cs
@@ -1,43 +1,129 @@
 using System.Collections;
-using System.Threading;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour
 {
     #region new_version
 
-    EventWaitHandle thd_exited = new EventWaitHandle(false, EventResetMode.ManualReset);
+    private GameObject _panel;  //child object
 
-    private string _currScene;
+    //current scene's dialog index number.
+    //this variable has counted up when calling the ReqViewDialog function.
+    private int _currIndex = 0; 
+
+    /* --- print event flag variables --- */
+    private bool b_pntFinish;  //print event finish flag vars.
+    private bool b_pntCut;     //print event cutting flag vars.
+
+    public Button btn_Skip;
 
     private void Awake()
     {
-        GameObject.Find("Panel").transform.gameObject.SetActive(false);
+        //_panel = GameObject.Find("Canvas_Textbox").transform.Find("Panel").gameObject;
+        _panel = this.transform.Find("Panel").gameObject;
+        b_pntFinish = false;
+
+        DialogEnabler(false); 
     }
 
-    public void ReqViewDialog(string sceneName)
+    private void Start()
     {
-        Thread dialog_thd;
-
-        _currScene = sceneName;
-
-        dialog_thd = new Thread(getCurrSceneDialog);
-        dialog_thd.Start((thd_exited));
+        btn_Skip.onClick.AddListener(SkipDialog);
     }
 
-    void getCurrSceneDialog(object obj)
+    private void DialogEnabler(bool Activity)
     {
-        GameManager obj_gm = GameManager.instance;
+        if(Activity) {
+            _panel.SetActive(true);
+        }else {
+            _panel.SetActive(false);
+        }
+    }
 
-        var events = (System.ValueTuple<EventWaitHandle>)obj;
-        {
-            obj_gm.dialog_db = obj_gm.xml_mgr.LoadDialogwithScene(_currScene, "Text.xml");
-            //xmlParser.xmlName = "/Text.xml";
-            //DialogList = xmlParser.LoadData(_sceneName);
+    public void ReqViewDialog()
+    {
+        List<string> textList = new List<string>();
+        Time.timeScale = 0; //stop the game.
 
-            events.Item1.Set();
+        //flag variables init.
+        b_pntCut = false;
+        b_pntFinish = false;
+
+        if(GameManager.instance.dialog_db.Index.Count > 0) {
+            //if exist valid dialog data, view on dialog.
+            _currIndex++;           //index count up.
+            for (int i = 0; i < GameManager.instance.dialog_db.Index.Count; i++) {
+                if(GameManager.instance.dialog_db.Index[i] == _currIndex) {
+                    textList.Add(GameManager.instance.dialog_db.Text[i]);
+                }
+            }
+            
+            DialogEnabler(true);    //dialog panel enable.
+            //Debug.Log(_panel.transform.Find("Text").gameObject.GetComponent<Text>().text);
+            StartCoroutine(printDialog(textList));
+            StartCoroutine(SkipEvtListener());
+        }
+    }
+
+    private IEnumerator printDialog(List<string> texts)
+    {
+        GameObject textBox = _panel.transform.Find("Text").gameObject;
+        string currShowString;
+
+        if (!b_pntFinish) {
+            foreach(string tmpString in texts) {
+                currShowString = "";
+
+                for(int i = 0; i < tmpString.Length; i++) {
+                    if (b_pntCut) {
+                        textBox.GetComponent<Text>().text = tmpString;
+                        break;
+                    }
+                    currShowString = tmpString.Substring(0, i + 1);
+                    textBox.GetComponent<Text>().text = currShowString;
+                    yield return new WaitForSecondsRealtime(0.1f);
+                }
+
+                if(b_pntCut) {
+                    yield return new WaitForSecondsRealtime(0.05f);
+                }else {
+                    yield return new WaitForSecondsRealtime(0.3f);
+                }
+            }
+
+            b_pntCut = false;
+            b_pntFinish = true;         //finish flag enable.
+
+            yield return new WaitForSecondsRealtime(1.0f);
+        }
+
+        //finished print dialog.
+        DialogEnabler(false);
+        Time.timeScale = 1.0f;          //re-operation this game.
+        StopCoroutine("printDialog");   //stop this coroutine.
+
+    }
+
+    private IEnumerator SkipEvtListener()
+    {
+        while(!b_pntFinish) {
+            if (Input.GetKey(KeyCode.Space)) {
+                SkipDialog();
+            }
+            yield return new WaitForEndOfFrame();
+        }
+
+        StopCoroutine("SkipEvtListener");
+    }
+
+    public void SkipDialog()
+    {
+        if(!b_pntCut) {
+            b_pntCut = true;
         }
     }
 
